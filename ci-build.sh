f389803b4b9d865e4bca4885418cdcc4a3e9f33e
Tom Hombergs <tom.hombergs@gmail.com>
2019-06-26 05:35:37 +0200

    set bundler version to 2.0.1 in travis.yml

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-gem install bundler --version 2.0.1
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --disable-external --allow-hash-href

d8bb6e12c6c6c259186b3c6733278123fbe5e4a0
Tom Hombergs <tom.hombergs@gmail.com>
2022-02-12 16:04:05 +1100

    updating ad links

--- themes/geeky/assets/plugins/popup/popups.js
+++ themes/geeky/assets/plugins/popup/popups.js
@@ -27,7 +27,7 @@ $( document ).ready(function() {
           variant: 10,
           slide: 1,
           position: "left",
-          content: "<a href=\"https://learn.launchdarkly.com/application-modernization-guide/?utm_source=reflectoring&utm_medium=display&utm_campaign=apmn&utm_content=app_modernization\"><img width=\"600\" alt=\"State of Feature Management\" src=\"/images/launchdarkly/2021_7_best_practice_short-term_and_permanent_feature_flags_Ad_1200x1200 (2).png\"/></a>",
+          content: "<a href=\"https://launchdarkly.com/?utm_source=reflectoring&utm_medium=display&utm_campaign=aodm&utm_content=ebook_oreilly_efm\"><img width=\"600\" alt=\"State of Feature Management\" src=\"/images/launchdarkly/2021_7_best_practice_short-term_and_permanent_feature_flags_Ad_1200x1200 (2).png\"/></a>",
           afterPopup: function() {
             setCookie("launchdarkly", "true", 7);
           }
